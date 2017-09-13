efd2d005c780fa2cce873539c8a51fca04118245
Subhasis <zippie.subhasis@gmail.com>
2017-09-13 10:08:22 +0530

    Added base62 strategy instead of MD5

---
+++ config/initializers/base62.rb
@@ -0,0 +1,23 @@
+class Base62
+  ENCODER = Hash.new do |h,k|
+    # Converts String to a Hash
+    Hash[ k.chars.map.with_index.to_a.map(&:reverse) ]
+  end
+  #<summary>
+  #This method generates shorten URL
+  # </summary>
+  # <param name="value">Epoch Token</param>
+  # <returns>Corresponding shorten URL</returns>
+  def self.encode( value )
+    # Constructing and avoing some words which makes confused
+    base62 = ([ *0..9, *'a'..'z', *'A'..'Z' ] - %w[i I 1 o u l O 0]).join
+    ring = ENCODER[base62]
+    base = base62.length
+    result = []
+    until value == 0
+      result << ring[ value % base ]
+      value /= base
+    end
+    result.reverse.join
+  end
+end
