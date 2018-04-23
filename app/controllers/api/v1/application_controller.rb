73a9318e779273fdb0caad07407cd46c11450904
SubhaPrince <subhasisgiri01@gmail.com>
2018-04-23 09:02:42 +0530

    setwithhost method added for set the host_with_port attaribte

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::ApplicationController < ActionController::API
+  before_action :set_host_with_port
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+
+  def set_host_with_port
+    Url.host_with_port = request.host_with_port
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_host_with_port
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def set_host_with_port
+    Url.host_with_port = request.host_with_port
+  end
 end
